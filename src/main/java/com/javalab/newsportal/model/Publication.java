59de5f4daba6f337d66c605a5b107199f26ccc8e
Aliya Altynbekova <Aliya_Altynbekova@epam.com>
2019-10-03 16:11:20 +0600

    NP-15 // hibernate annotations(mapped superclass)

--- src/main/java/com/javalab/newsportal/model/Publication.java
+++ src/main/java/com/javalab/newsportal/model/Publication.java
@@ -1,15 +1,22 @@
 package com.javalab.newsportal.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@MappedSuperclass
 public abstract class Publication {
 
     public static final String ID = "id";
     public static final String CONTENT = "content";
     public static final String CREATION_DATE = "creationDate";
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "publication_seq")
+    @SequenceGenerator(name = "publication_seq", sequenceName = "publication_seq", allocationSize = 1)
     private Long id;
+    @Column(name = "cr_date", nullable = false)
     private LocalDateTime creationDate;
+    @Column(nullable = false)
     private String content;
 
 
